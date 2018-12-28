8ed47d78b60c48d8b3655c1a791066f53f671319
wanghao <wanghao.pivot@bytedance.com>
2018-12-28 14:51:45 +0800

    test:commit1
    Change-Id: Ief62a2d6bac431617617c6c7be092708be98421c

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Routes from './router';
 import {browserHistory, Router} from 'react-router';
 
+//  commit1
 ReactDOM.render(
   <Router history={browserHistory} routes={Routes}/>, document.getElementById('root')
 );
